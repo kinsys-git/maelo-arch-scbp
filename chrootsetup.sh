2e90b37573401c13b71957f59e8dc9e0f9a6b170
maelodic <bradynwatson@gmail.com>
2018-02-17 02:20:02 +0000

    compile kernel from scratch

--- chrootsetup.sh
+++ chrootsetup.sh
@@ -169,23 +169,31 @@ userandrootpasswd() {
 }
 
 scbp() {
-	cd /root
-	mkdir tmp/
-	cd tmp/
-	svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/usr/
-	svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/boot/
-	svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/etc/
-	svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/lib/
-	cp -RT usr/ /usr/
-	cp -RT boot/ /boot/
-	cp -RT etc/ /etc/
-	cp -RT lib/ /lib/
-	pacaur -S xkeyboard-config-chromebook --noconfirm --noedit
-	systemctl enable caroline-audio
-	mkinitcpio -p linux
-	grub-mkconfig -o /boot/grub/grub.cfg
-	cd ..
-	rm -rf tmp/
+        cd /root
+        mkdir tmp/
+        cd tmp/
+        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/usr/
+        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/etc/
+        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/lib/
+        cp -RT usr/ /usr/
+        cp -RT etc/ /etc/
+        cp -RT lib/ /lib/
+        git clone https://github.com/galliumos/linux
+        wget https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/atmel_mxt_ts.c
+	cp atmel_mxt_ts.c linux/drivers/touchpad/atmel_mxt_ts.c
+	cd linux
+        yes "" | make localmodconfig >/dev/null
+        make
+        make install
+        make modules_install
+        cp arch/x86/boot/bzimage /boot/vmlinuz
+        cp arch/x86/boot/bzimage /boot/vmlinuz-linux
+        systemctl enable caroline-audio
+        mkinitcpio -p linux
+        grub-mkconfig -o /boot/grub/grub.cfg
+        cd ..
+        rm -rf tmp/
+        pacaur -S xkeyboard-config-chromebook --noconfirm --noedit
 }
 
 # Main Function
