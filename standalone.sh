353fa60ab61d5fbef07efabc368c674ef8322fe6
maelodic <bradynwatson@gmail.com>
2018-02-24 19:41:38 +0000

    linking audio interface

--- standalone.sh
+++ standalone.sh
@@ -4,31 +4,38 @@ scbp() {
         cd /root
         mkdir tmp/
         cd tmp/
-        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/usr/
-        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/etc/
-        svn checkout https://github.com/maelodic/maelo-arch-scbp/trunk/lib/
-        cp -RT usr/ /usr/
-        cp -RT etc/ /etc/
-        cp -RT lib/ /lib/
+        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/usr/
+        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/etc/
+        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/lib/
+        rsync -a usr/ /usr/
+        rsync -a etc/ /etc/
+        rsync -a lib/ /lib/
 	git clone https://github.com/galliumos/linux
 	wget https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/atmel_mxt_ts.c
         cp atmel_mxt_ts.c linux/drivers/touchpad/atmel_mxt_ts.c
+        wget https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/galliumos-init-skylake
+	chmod +x galliumos-init-skylake
+	wget https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/galliumos.preset
+	cp galliumos.preset /etc/mkinitcpio.d/galliumos.preset
+	cp galliumos-init-skylake /usr/bin/galliumos-init-skylake
 	cd linux
-	yes "" | make localmodconfig >/dev/null
-	make
-	make install
-	make modules_install
-	cp arch/x86/boot/bzimage /boot/vmlinuz
-	cp arch/x86/boot/bzimage /boot/vmlinuz-linux
-        systemctl enable caroline-audio
-        mkinitcpio -p linux
+	make localmodconfig -j4
+	make -j4
+	make -j4 modules_install
+	make -j4 install
+	cp arch/x86/boot/bzimage /boot/vmlinuz-galliumos
+        systemctl enable galliumos-skylake
+        mkinitcpio -p galliumos
         grub-mkconfig -o /boot/grub/grub.cfg
         cd ..
         rm -rf tmp/
-        pacaur -S xkeyboard-config-chromebook --noconfirm --noedit	
 }
 
 main() {
+	if [[ $EUID -ne 0 ]]; then
+   		echo "This script must be run as root" 
+   		exit 1
+	fi
 	scbp
 }
 
