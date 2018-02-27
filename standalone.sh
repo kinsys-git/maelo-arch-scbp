267ce050de6414b98ed6d435d3816b7e3e010a88
maelodic <bradynwatson@gmail.com>
2018-02-27 10:38:09 -0800

    First braswell script

--- standalone.sh
+++ standalone.sh
@@ -5,35 +5,28 @@ scbp() {
    		echo "This script must be run as root" 
 		exit 1
 	fi
-        mkdir /root/tmp/
-        cd /root/tmp/
-        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/usr/
-        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/etc/
-        svn checkout https://github.com/galliumos/galliumos-skylake/trunk/lib/
+	mkdir /root/tmp
+	cd /root/tmp
+	git clone -b braswell https://github.com/maelodic/maelo-arch-scbp/
+	cd /root/tmp/maelo-arch-scbp
+	chmod +x usr/bin/*
         rsync -a usr/ /usr/
         rsync -a etc/ /etc/
         rsync -a lib/ /lib/
-	ln -s /usr/share/alsa/ucm/sklnau8825adi /usr/share/alsa/ucm/Google-Caroline-1.0-Caroline
-	ln -s /usr/share/alsa/ucm/sklnau8825adi.conf /usr/share/alsa/ucm/Google-Caroline-1.0-Caroline/Google-Caroline-1.0-Caroline.conf
 	git clone https://github.com/galliumos/linux
-	wget -P /root/tmp/ https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/atmel_mxt_ts.c
-        cp /root/tmp/atmel_mxt_ts.c /root/tmp/linux/drivers/input/touchscreen/atmel_mxt_ts.c
-        wget -P /root/tmp/ https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/galliumos-init-skylake
-	chmod +x galliumos-init-skylake
 	wget -P /root/tmp/ https://raw.githubusercontent.com/maelodic/maelo-arch-scbp/master/galliumos.preset
 	cp /root/tmp/galliumos.preset /etc/mkinitcpio.d/galliumos.preset
-	cp /root/tmp/galliumos-init-skylake /usr/bin/galliumos-init-skylake
 	cd /root/tmp/linux
 	make localmodconfig
 	make -j4
 	make -j4 modules_install
 	make -j4 install
 	cp /root/tmp/linux/arch/x86/boot/bz* /boot/vmlinuz-galliumos
-        systemctl enable galliumos-skylake
+        systemctl enable galliumos-braswell
         mkinitcpio -p galliumos
         grub-mkconfig -o /boot/grub/grub.cfg
         cd ..
-        #rm -rf /root/tmp/
+        rm -rf /root/tmp/
 }
 
 main() {
